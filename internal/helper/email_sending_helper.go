f6edfff99b6d36e4d79e7a711130527ba3e48596
wicaker <wicaksanaigede@gmail.com>
2020-07-26 17:16:56 +0800

    add email_sending_helper

---
+++ internal/helper/email_sending_helper.go
@@ -0,0 +1,43 @@
+package helper
+
+import (
+	"context"
+	"time"
+
+	"github.com/wicaker/notification/internal/domain"
+	"github.com/wicaker/notification/internal/pkg/mail"
+
+	"gopkg.in/gomail.v2"
+)
+
+type emailSendingHelper struct {
+	contextTimeout time.Duration
+	dialer         *gomail.Dialer
+}
+
+// NewEmailSendingHelper will create new an emailSendingHelper object representation of domain.EmailSendingHelper interface
+func NewEmailSendingHelper(timeout time.Duration) domain.EmailSendingHelper {
+	return &emailSendingHelper{
+		contextTimeout: timeout,
+		dialer:         mail.NewDialer(),
+	}
+}
+
+func (eu *emailSendingHelper) Send(ctx context.Context, email *domain.EmailSending) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", email.EmailFrom)
+	m.SetHeader("To", email.EmailTo[0])
+	m.SetHeader("Subject", email.Subject)
+	m.SetBody("text/html", email.HTMLMessage)
+
+	if email.Attach != "" {
+		m.Attach(email.Attach)
+	}
+
+	err := eu.dialer.DialAndSend(m)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
