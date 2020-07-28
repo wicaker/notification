1da239d9d33df4c6fdf198854a8e4216cf83525a
wicaker <wicaksanaigede@gmail.com>
2020-07-28 18:46:01 +0800

    set up main.go

--- cmd/notification/main.go
+++ cmd/notification/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
 	"time"
+
+	"github.com/wicaker/notification/config"
+	"github.com/wicaker/notification/internal/transport"
 
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -16,4 +22,44 @@ func init() {
 	}
 }
 
-func main() {}
+func main() {
+	var (
+		gomail     = config.NewGomailDialer()
+		rabbitConn = config.NewRabbitmq()
+		errChan    = make(chan error)
+	)
+	defer close(errChan)
+	defer func() {
+		err := rabbitConn.Close()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"at": time.Now().Format("2006-01-02 15:04:05"),
+			}).Errorln(err)
+		}
+	}()
+
+	go func() {
+		for {
+			transport.Handler(rabbitConn.Queue, gomail)
+
+			// reconnect when rabbitmq server terminated accidentally
+			err := <-rabbitConn.ErrorChannel
+			if !rabbitConn.IsClose {
+				rabbitConn.Reconnect(err)
+			} else {
+				errChan <- err
+			}
+
+		}
+	}()
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	logrus.WithFields(logrus.Fields{
+		"at": time.Now().Format("2006-01-02 15:04:05"),
+	}).Errorln("terminated ", <-errChan)
+}
