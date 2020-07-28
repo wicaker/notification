f2afeeb8fca2688846914b9eaa089f646acbca67
wicaker <wicaksanaigede@gmail.com>
2020-07-28 17:41:55 +0800

    remove LogMqError func

--- internal/transport/handler.go
+++ internal/transport/handler.go
@@ -3,31 +3,17 @@ package transport
 import (
 	"time"
 
+	"github.com/wicaker/notification/internal/domain"
 	"github.com/wicaker/notification/internal/helper"
 	"github.com/wicaker/notification/internal/pkg/rmq"
 	"github.com/wicaker/notification/internal/usecase"
-
-	"github.com/sirupsen/logrus"
-	"github.com/streadway/amqp"
 )
 
-// LogMqError to log amqp error
-func LogMqError(err error, msg *amqp.Delivery, message string) {
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"at": time.Now().Format("2006-01-02 15:04:05"),
-		}).Printf("%s. Message from routingKey: %s rejected...", err, msg.RoutingKey)
-		msg.Reject(false)
-	} else {
-		msg.Ack(true)
-	}
-}
-
 // Handler /
-func Handler(rmqQueue []rmq.Queue) {
+func Handler(rmqQueue []rmq.Queue, dialer domain.GomailDialer) {
 	timeoutContext := time.Duration(2) * time.Second
 
-	emailSendingHelper := helper.NewEmailSendingHelper(timeoutContext)
+	emailSendingHelper := helper.NewEmailSendingHelper(timeoutContext, dialer)
 	emailSendingUsecase := usecase.NewEmailSendingUsecase(timeoutContext, emailSendingHelper)
 	NewEmailSendingHandler(rmqQueue, emailSendingUsecase)
 }
